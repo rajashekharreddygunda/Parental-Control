7636b12c36ffd45f0e92f9c1dc089a28b188719f
rajashekharreddygunda <145490850+rajashekharreddygunda@users.noreply.github.com>
2026-01-06 22:41:56 +0530

    Create hx711_driver_code.cpp

---
+++ hx711_driver_code.cpp
@@ -0,0 +1,68 @@
+#include "mbed.h"
+
+DigitalOut pd_sck(PA_5);
+DigitalIn dout(PA_4);
+
+void pwr_down(){
+    pd_sck = 1;
+    wait_us(70);
+}
+
+bool is_ready(){
+    if(dout==0){
+        return 1;
+    }
+    return 0;
+}
+
+bool* data_receive(unsigned char resolution = 128){
+    bool* result = new bool[24];
+    if(is_ready()){
+        for(int i=0; i<24; i++){
+            pd_sck =1;
+            wait_us(10);
+            result[i] = dout;
+            wait_us(15);
+            pd_sck =0;
+            wait_us(10);
+        }
+        if (resolution == 128){
+            pd_sck = 1;
+            wait_us(35);
+            pd_sck = 0;
+            wait_us(35);
+        }
+        else if (resolution == 32){
+            pd_sck = 1;
+            wait_us(35);
+            pd_sck = 0;
+            wait_us(35);
+            pd_sck = 1;
+            wait_us(35);
+            pd_sck = 0;
+            wait_us(35);
+        }
+        else if (resolution == 64){
+            pd_sck = 1;
+            wait_us(35);
+            pd_sck = 0;
+            wait_us(35);
+            pd_sck = 1;
+            wait_us(35);
+            pd_sck = 0;
+            wait_us(35);
+            pd_sck = 1;
+            wait_us(35);
+            pd_sck = 0;
+            wait_us(35);
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+  while(1) {
+    is_ready();
+  }
+}
